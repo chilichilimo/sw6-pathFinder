e7cd7fb5d4e69da8836b40a81369450de5c462d4
chilichilimo <afshar.m.95@gmail.com>
2017-02-20 17:54:16 +0000

    adding Solution1

---
+++ src/scenario6/Solution1.java
@@ -0,0 +1,19 @@
+package scenario6;
+
+/**
+ * Created by Afshar on 20/02/2017.
+ * This file includes the first solution to scenario week 6 problem.
+ */
+public class Solution1 {
+    //Make visibility graph from Question.
+    //FIXME: Figure out output of the graphCreator skeleton.
+    void graphCreator(Question question){
+
+    }
+
+    //FIXME: Figure out output of findCoords. **Input will be output of graphCreator.
+    void findCoords(){
+
+    }
+
+}
