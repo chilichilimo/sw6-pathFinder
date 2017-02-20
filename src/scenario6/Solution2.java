d1c6a693ff5938371e5042f5870933003cfe5faf
robobot97 <arjunkhurana26@hotmail.com>
2017-02-20 22:03:38 +0000

    added haveIntersection thing

--- src/scenario6/Solution2.java
+++ src/scenario6/Solution2.java
@@ -15,7 +15,18 @@ public class Solution2 {
     //FIXME: Implement this.
     //Checks if two lines have intersection.
     Boolean haveIntersection(Line2D l1, Line2D l2){
-        Boolean result = false;
+
+        Line2D line1 = new Line2D.Float(100, 100, 200, 200);
+        Line2D line2 = new Line2D.Float(150, 150, 150, 200);
+        boolean result = line2.intersectsLine(line1);
+        System.out.println(result); // => true
+
+// Also check out linesIntersect() if you do not need to construct the line objects
+// It will probably be faster due to putting less pressure on the garbage collector
+// if running it in a loop
+        System.out.println(Line2D.linesIntersect(100,100,200,200,150,150,150,200));
+
+        //Boolean result = false;
         return result;
     }
 
