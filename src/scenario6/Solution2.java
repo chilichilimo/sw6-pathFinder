cb35ff80487cdaa4488a9dfe0e2aa3fdcadaa0da
chilichilimo <afshar.m.95@gmail.com>
2017-02-20 21:23:14 +0000

    Adding initial skeleton

---
+++ src/scenario6/Solution2.java
@@ -0,0 +1,27 @@
+package scenario6;
+
+import java.awt.geom.Line2D;
+
+/**
+ * Created on 20/02/2017.
+ * Freeze-Tag part: Naive. A->B->C->...
+ * Robot-Motion-Planning: Over every iteration, if AB passes through any line of any obstacle, apply the algorithm to the
+ */
+
+public class Solution2 {
+
+    //TODO: Implement this.
+    Boolean haveIntersection(Line2D l1, Line2D l2){
+        Boolean result = false;
+        return result;
+    }
+
+    //TODO: Implement this.
+    Boolean checkIntersectionWithObstacles(Line2D line){
+        Boolean result = false;
+        return result;
+    }
+
+    
+
+}
