8f55989e81d599795acba964cb12b31cfd346d36
Jay Jeyaruban <jay.jeyaruban.15@ucl.ac.uk>
2017-02-20 16:46:36 +0000

    Added obstacle class.

---
+++ src/scenario6/Obstacle.java
@@ -0,0 +1,19 @@
+package scenario6;
+
+import java.awt.geom.Point2D;
+import java.util.ArrayList;
+
+/**
+ * Created by Jay on 20/02/2017.
+ */
+public class Obstacle {
+	ArrayList<Point2D> vertices;
+
+	public Obstacle() {
+
+	}
+
+	public ArrayList<Point2D> getVertices() {
+		return vertices;
+	}
+}
