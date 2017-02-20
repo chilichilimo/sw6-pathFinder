72d6aae73a93257ce19a8b15748cec6d22bc5a9c
Jay Jeyaruban <jay.jeyaruban.15@ucl.ac.uk>
2017-02-20 16:37:45 +0000

    Modified overall structure.

--- src/scenario6/Main.java
+++ src/scenario6/Main.java
@@ -1,11 +1,30 @@
 package scenario6;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Main {
+
+    private static void solve(Question question) {
+        // Write algorithm here
+    }
 
     public static void main(String[] args) {
 	// write your code here
+        File file;
+        Scanner scanner;
 
-        RobotsParser parser = new RobotsParser("robots.mat");
-        parser.getQuestion();
+        try {
+            file = new File("robots.mat");
+            scanner = new Scanner(file);
+
+            while (scanner.hasNextLine()) {
+                String q = scanner.nextLine();
+                Question question = new Question(q);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
---
+++ src/scenario6/Question.java
@@ -0,0 +1,45 @@
+package scenario6;
+
+import java.awt.*;
+import java.awt.geom.Point2D;
+import java.util.ArrayList;
+
+/**
+ * Created by Jay on 20/02/2017.
+ */
+public class Question {
+	ArrayList<Point2D> robots;
+	ArrayList<ArrayList<Point>> obstacles;
+	String question;
+
+	public Question(String question) {
+		this.question = question;
+	}
+
+	public ArrayList<Point> getRobots() {
+		return robots;
+	}
+
+	public ArrayList<ArrayList<Point>> getObstacles() {
+		return obstacles;
+	}
+
+	private void setQuestion() {
+		int robotSeparator = question.indexOf('#');
+		if (robotSeparator > -1) {
+			currentQuestionHasObstacle = true;
+			System.out.println(question.substring(0, robotSeparator));
+		}
+
+		String tmp = question.substring(robotSeparator + 1);
+		currentQuestion = tmp;
+	}
+
+	private void setRobots(String robotString) {
+
+	}
+
+	private void setObstacles(String obstacleString) {
+
+	}
+}
