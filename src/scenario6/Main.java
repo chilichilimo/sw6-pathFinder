8b2c4785354f5ce7d591c33ee2d1666bc53ed507
Jay Jeyaruban <jay.jeyaruban.15@ucl.ac.uk>
2017-02-20 15:14:13 +0000

    Push cause I have to.

---
+++ src/scenario6/Main.java
@@ -0,0 +1,11 @@
+package scenario6;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        RobotsParser parser = new RobotsParser("robots.mat");
+        parser.getQuestion();
+    }
+}
