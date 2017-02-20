8506e7e53164c68aff0510dc5faac47f5483bc5b
chilichilimo <afshar.m.95@gmail.com>
2017-02-20 19:05:12 +0000

    adding graph skeleton

---
+++ src/scenario6/Graph.java
@@ -0,0 +1,13 @@
+package scenario6;
+
+/**
+ * Created by Afshar on 20/02/2017.
+ * This class is used in Solution one to create visibility graph.
+ */
+public class Graph {
+
+
+    void dijkstra(){
+
+    }
+}
